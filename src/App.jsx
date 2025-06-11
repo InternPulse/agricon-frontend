88a81123895bfb24d3ff78cc6e06932c2bbcd255
unknown <taiwokolapo533@gmail.com>
2025-06-11 15:26:51 +0100

    otp page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 //how to use the images from the assets.js file
 //import the directory below
 import { assets } from "./assets/assets";
+import OTP from "./page/OTP";
 
 
 function App() {
   
 
   return (
-    <div className='text-red-700 text-3xl flex items-center justify-center min-h-screen'>
-      <div>
-          <div>
-      {/* render it this way in your various components */}
-              <img src={assets.agriconLogo} alt="agriCon"/>
-          </div>
-          <div>Welcome to Agricon</div>
-          </div>
-      </div>
+    <div>
+      <OTP />
+    </div>
   )
 }
 
