db072468cbbb0ed499bd5063bda466b434c8c46b
Gogo602 <gogomarosi61@gmail.com>
2025-06-11 22:55:52 +0300

    File src/assets; added and exported assets files

---
+++ src/assets/assets.js
@@ -0,0 +1,10 @@
+import agriconLogo from './agriconLogo.png'
+import authImage from './authImage.png'
+import otp from './otp.png'
+
+
+export const assets = {
+    agriconLogo,
+    authImage,
+    otp,
+}
