79b69fd490e94041adce4921daf12c0521346343
Vicky041 <osemekev22@gmail.com>
2025-06-12 00:09:55 +0300

    login section

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
 //how to use the images from the assets.js file
 //import the directory below
-import { assets } from "./assets/assets";
 
+import Login from "./components/Login";
 
 function App() {
-  
-
   return (
-    <div className='text-red-700 text-3xl flex items-center justify-center min-h-screen'>
+    <div className="text-red-700 text-3xl flex items-center justify-center min-h-screen">
       <div>
-          <div>
-      {/* render it this way in your various components */}
-              <img src={assets.agriconLogo} alt="agriCon"/>
-          </div>
-          <div>Welcome to Agricon</div>
-          </div>
-      </div>
-  )
-}
+        <div>{/* render it this way in your various components */}</div>
 
+        <Login />
+      </div>
+    </div>
+  );
+}
 
 export default App;
