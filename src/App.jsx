7a0ce9bfe3c1cdca1c3eeeae052d27b99a4d454b
Gogo602 <gogomarosi61@gmail.com>
2025-06-11 22:36:57 +0300

    Updated Main branch

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+
+  return (
+    <div className='text-red-700 text-3xl flex items-center justify-center min-h-screen'>
+      <div>Welcome to Agricon</div>
+    </div>
+  )
+}
+
+
+export default App;
