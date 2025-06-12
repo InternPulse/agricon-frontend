3a6fc1cc7be81b463d8d94013b4278ad68c95c49
Gogo602 <gogomarosi61@gmail.com>
2025-06-12 23:57:08 +0300

    File: App.jsx; completed merge from raphael/branch & set route path

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import AgriconHome from './pages/AgriconHome'
 import SignupLanding from './pages/SignupLanding'
 import PasswordReset from './pages/PasswordReset'
 import OTP from "./pages/OTP";
+import SignUp from "./pages/SignUp";
+
 
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
     <Routes>
         <Route path='/' element={<AgriconHome />}/>
         <Route path='/roles' element={<SignupLanding />}/>
+        <Route path='/signup' element={<SignUp />}/>
         <Route path='/reset-password' element={<PasswordReset />}/>
         <Route path='/otp' element={<OTP />}/>
     </Routes>
