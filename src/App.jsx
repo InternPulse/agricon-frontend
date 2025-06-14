114ede3af0872dc94cf9422603c2bd219ffa2c5b
Gogo602 <gogomarosi61@gmail.com>
2025-06-15 00:12:56 +0300

    File:components/bookingsCreation&Management & pages/bookingsCreation&Management; added folder and some files for bookings Creation Management

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import PasswordReset from './pages/PasswordReset';
 import Form from './components/form';
+import Cards from './pages/Cards';
 
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
     <div>
       <Routes>
           {/* reset-password */}
-          <Route path='/' element={<PasswordReset />}/>
+          <Route path='/' element={<Cards />}/>
+          <Route path='/password-reset' element={<PasswordReset />}/>
       </Routes>
     </div>
   )
---
+++ src/components/bookingCreateion&Management/Card.jsx
@@ -0,0 +1,11 @@
+
+
+function Card({children}) {
+    return (
+        <Card className="border rounded-md p-5">
+            {children}
+        </Card>
+    )
+}
+
+export default Card;
