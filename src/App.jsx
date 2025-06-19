4232c83928c9a16fd7e5030a5bf0375563c6a5aa
Gogo602 <gogomarosi61@gmail.com>
2025-06-20 02:49:43 +0300

    Revert "feat: Add performance card and sidebar component; update assets and dashboard layout"
    This reverts commit 6822e330b44a6cf66dd28d57422fd307d89771eb.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 //how to use the images from the assets.js file
 //import the directory below
 import { assets } from "./assets/assets";
-import FacilityOwnerDashboard from "./pages/Dassss";
+
 
 function App() {
-  return <FacilityOwnerDashboard />
+  
+
+  return (
+    <div className='text-red-700 text-3xl flex items-center justify-center min-h-screen'>
+      <div>
+          <div>
+      {/* render it this way in your various components */}
+              <img src={assets.agriconLogo} alt="agriCon"/>
+          </div>
+          <div>Welcome to Agricon</div>
+          </div>
+      </div>
+  )
 }
+
 
 export default App;
--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,12 +1,10 @@
 import agriconLogo from './agriconLogo.png'
-import agriconLogoSvg from './agriconLogo.svg'
 import authImage from './authImage.png'
 import otp from './otp.png'
 
 
 export const assets = {
     agriconLogo,
-    agriconLogoSvg,
     authImage,
     otp,
 }
