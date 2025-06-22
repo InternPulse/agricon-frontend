201ff2b295325f51e7d6da4bb2afb381e8ba0cf8
Gogo602 <gogomarosi61@gmail.com>
2025-06-22 18:17:39 +0300

    File:components/sidebar; added sidebar for farmers dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
 //how to use the images from the assets.js file
 //import the directory below
+import { Route, Routes } from "react-router-dom";
 import { assets } from "./assets/assets";
+import Sidebar from "./components/sideBar";
+import Sample from "./pages/samplePage";
 
+
+function Overview() {
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen">
+        <div>
+          <h1>Overview page for Dashboard</h1>
+          <img src={assets.agriconLogo} alt="agriCon" />
+          <div>Welcome to Agricon</div>
+        </div>
+      </div>
+    </>
+  );
+}
 
 function App() {
   
 
   return (
-    <div className='text-red-700 text-3xl flex items-center justify-center min-h-screen'>
-      <div>
-          <div>
-      {/* render it this way in your various components */}
-              <img src={assets.agriconLogo} alt="agriCon"/>
-          </div>
-          <div>Welcome to Agricon</div>
+    <>
+      <div className="flex w-full">
+        <Sidebar />
+          <div className="pl-68 h-[200%]">
+          <Routes>
+             {/* render it this way in your various components */}
+              <Route path='/' element={<Overview />}/>
+              <Route path='/facilities' element={<Sample/>}/>
+            </Routes>
           </div>
       </div>
+    </>
   )
 }
 
---
+++ src/pages/samplePage.jsx
@@ -0,0 +1,13 @@
+
+
+function Sample(){
+    return(
+        <>
+            <div className="flex items-center justify-center min-h-screen">
+                This is Facility page
+            </div>
+        </>
+    );
+}
+
+export default Sample;
