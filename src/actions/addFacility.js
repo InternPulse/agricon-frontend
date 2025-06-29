bf909ebb0c162e0f77dedbc9ebcf767787f2b737
Gogo Benson <151610817+Gogo602@users.noreply.github.com>
2025-06-29 20:06:24 +0300

    File: addFacility; removed testing bearer token

--- src/actions/addFacility.js
+++ src/actions/addFacility.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
 export const addFacilities = async (facilityData) => { 
+  const accessToken = localStorage.getItem("access_token");
   try {
     const response = await axios.post(
       'https://agricon-express-backend.onrender.com/api/v1/facilities/', 
       facilityData, 
       { 
         headers: {
-          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzUxMTk1NDc4LCJpYXQiOjE3NTExNTk0NzgsImp0aSI6ImQwNDllMjk5NzFmNjQzMjU4OWY5MWNkY2U5YmJiYjE3IiwidXNlcl9pZCI6IjIyYWEyYjMwLTc1NWQtNDFlNC05NWY4LTZmZjc3ZjJmMGNlOCIsImVtYWlsIjoibWFqZWZhbW91c0BnbWFpbC5jb20iLCJyb2xlIjoiT1BFUkFUT1IifQ.e9gMHoIiCrau8XWaRiOQEGqQTDnOHOmx7UvqnMlD6VE', 
+          'Authorization': `Bearer ${accessToken}` ;
         }
       }
     );
