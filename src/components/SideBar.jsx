ee208c21dd72d99f6efd72af4fbaac45062863a0
unknown <taiwokolapo533@gmail.com>
2025-07-05 07:30:31 +0100

    updated

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -7,7 +7,7 @@ const SideBar = () => {
     <div className='w-[15%] min-h-screen  top-0 left-0 md:block hidden bg-[#02402D] py-10  '>
 <div className=' px-  flex flex-col  gap-2 items-center   text-white  '>
     <Link><h1>Setting</h1></Link>
-    <Link to='/booking'><h1>Booking</h1></Link>
+    <Link to='/emptyfacility'><h1>Facility</h1></Link>
     </div>
     </div>
     
