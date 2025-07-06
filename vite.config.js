085f6871f2093e9db6439b65e4f0c510eba1a51e
Gogo Benson <151610817+Gogo602@users.noreply.github.com>
2025-07-06 19:17:12 +0100

    Added internmap vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,11 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      external: ['internmap']
+    },
+  },
   server: {
     port: 5173,
     proxy: {
