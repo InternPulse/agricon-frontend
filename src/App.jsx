99ca6108c91c02a0a26bfb6d76b61e61c300c50a
Vicky041 <osemekev22@gmail.com>
2025-07-08 22:47:48 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-//how to use the images from the assets.js file
-//import the directory below
+// //how to use the images from the assets.js file
+// //import the directory below
 
-import Booking from "./components/Facility-Details_View/Booking";
-import FacilityDetails from "./components/Facility-Details_View/FacilityView";
-import Navbar from "./components/Facility-Details_View/Nav";
-import './App.scss';
-import BookingSummary from "./components/Facility-Details_View/Booking-Summary";
-function App() {
-  return (
-    <div className="text-gray-700 text-3xl  justify-center min-h-screen">
-      <div>
-        <Navbar/>
+// import Booking from "./components/Facility-Details_View/Booking";
+// import FacilityDetails from "./components/Facility-Details_View/FacilityView";
+// import Navbar from "./components/Facility-Details_View/Nav";
+// import './App.scss';
+// import BookingSummary from "./components/Facility-Details_View/Booking-Summary";
+// function App() {
+//   return (
+//     <div className="text-gray-700 text-3xl  justify-center min-h-screen">
+//       <div>
+//         <Navbar/>
 
-        <FacilityDetails/>
-        <Booking/>
-        <BookingSummary/>
-      </div>
-    </div>
-  );
-}
+//         <FacilityDetails/>
+//         <Booking/>
+//         <BookingSummary/>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
