420a465d9579b1a86b21f28dfa45703fa29551fa
Transpose <tofexzyno@gmail.com>
2025-07-12 20:20:40 +0100

    Refactor DeleteConfirmationModal import path and create DeleteConfirmationModal component

--- src/components/NotificationComponents/IsNotification/Cards copy.jsx
+++ src/components/NotificationComponents/IsNotification/Cards copy.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { assets } from "../../../assets/assets";
-import DeleteConfirmationModal from "../ActiveNotificationBar/DeleteConfirmationModal";
+import DeleteConfirmationModal from "./DeleteConfirmationModal";
 
-export default function Cardss({ data, showModal, handleDelete, setShowModal }) {
-
+export default function Cardss({
+  data,
+  showModal,
+  handleDelete,
+  setShowModal,
+}) {
   return (
     <div className="mee">
       {data.map((data) => {
-        <div>me</div>
+        <div>me</div>;
       })}
     </div>
   );
--- src/components/NotificationComponents/IsNotification/Cards.jsx
+++ src/components/NotificationComponents/IsNotification/Cards.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { assets } from "../../../assets/assets";
-import DeleteConfirmationModal from "../ActiveNotificationBar/DeleteConfirmationModal";
+import DeleteConfirmationModal from "./DeleteConfirmationModal";
 
 export default function Cards({ data, showModal, handleDelete, setShowModal }) {
   return (
